cf18b23305b678152268b01243de281a5e750173
river-ranz <river.ranz@mymail.champlain.edu>
2026-04-12 16:27:55 -0400

    added basic client/server functions

--- LobbyAndMatchmaking/lobby-matchmaking/Client.h
+++ LobbyAndMatchmaking/lobby-matchmaking/Client.h
@@ -3,37 +3,83 @@
 #include <boost/asio.hpp>
 #include <iostream>
 #include <string>
+#include <thread>
+
+using boost::asio::ip::tcp;
+
+constexpr size_t CLIENT_BUFFER_SIZE = 1200;
+
+
+class TcpChatClient {
+public:
+    explicit TcpChatClient(boost::asio::io_context& io_context)
+        : m_io_context(io_context)
+        , m_socket(io_context)
+        , m_running(false)
+    {
+    }
+
+    bool is_connected() const {
+        return m_socket.is_open();
+    }
+
+    bool connect([[maybe_unused]] const std::string& host,
+                 [[maybe_unused]] uint16_t port) {
+        m_socket = tcp::socket(m_io_context);
+        tcp::resolver resolver(m_io_context);
+        auto endpoints = resolver.resolve(host, std::to_string(port));
+        try {
+            boost::asio::connect(m_socket, endpoints);
+        } catch (std::exception &e) {
+            return false;
+        }
+        return true;
+    }
+
+    void run() {
+        if (!is_connected()) {
+            std::cerr << "[Client] Not connected to server\n";
+            return;
+        }
+        m_running = true;
+    }
+
+    void disconnect() {
+        if (is_connected()) {
+            boost::system::error_code ec;
+            m_socket.shutdown(tcp::socket::shutdown_both, ec);
+            if (ec) { std::cerr << "Shutdown error: " << ec.message() << std::endl; }
+
+            m_socket.close(ec);
+            if (ec) { std::cerr << "Close error: " << ec.message() << std::endl; }
+        }
+        m_running = false;
+    }
+
+private:
+    boost::asio::io_context& m_io_context;
+    tcp::socket m_socket;
+    std::string m_username;
+    std::atomic<bool> m_running;
+};
 
 inline int run_chat_client(const std::string& host, uint16_t port) {
     try {
-        //boost::asio::io_context io_context;
-        //TcpChatClient client(io_context);
+        boost::asio::io_context io_context;
+        TcpChatClient client(io_context);
 
         std::cout << "[Client] TCP Chat Client\n";
         std::cout << "[Client] Connecting to " << host << ":" << port << "...\n";
 
-        /*if (!client.connect(host, port)) {
+        if (!client.connect(host, port)) {
             std::cerr << "[Client] Failed to connect to server\n";
             return 1;
         }
 
         std::cout << "[Client] Connected!\n";
 
-        // Get username from user
-        std::string username;
-        std::cout << "Enter your username: ";
-        std::getline(std::cin, username);
 
-        if (username.empty()) {
-            username = "anonymous";
-        }
-
-        client.set_username(username);
-        std::cout << "[Client] Joined as '" << username << "'\n";
-        std::cout << "[Client] Type messages and press Enter to send.\n";
-        std::cout << "[Client] Type /quit to exit.\n\n";
-
-        client.run();*/
+        client.run();
 
         return 0;
 
--- LobbyAndMatchmaking/lobby-matchmaking/LobbyServer.h
+++ LobbyAndMatchmaking/lobby-matchmaking/LobbyServer.h
@@ -3,19 +3,133 @@
 #include <boost/asio.hpp>
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <memory>
+#include <mutex>
+#include <thread>
+
+using boost::asio::ip::tcp;
+
+constexpr size_t MAX_MESSAGE_LENGTH = 1200;
+
+
+class ClientSession : public std::enable_shared_from_this<ClientSession> {
+public:
+    ClientSession(tcp::socket socket)
+        : m_socket(std::move(socket))
+    {
+    }
+
+    tcp::socket& socket() { return m_socket; }
+    const std::string& username() const { return m_username; }
+    void set_username(const std::string& name) { m_username = name; }
+
+private:
+    tcp::socket m_socket;
+    std::string m_username;
+};
+
+using ClientPtr = std::shared_ptr<ClientSession>;
+
+class UserRegistry {
+public:
+    bool add_user([[maybe_unused]] const std::string& username,
+                  [[maybe_unused]] ClientPtr client) {
+        std::lock_guard<std::mutex> lock(m_mutex);
+
+        if (m_users.contains(username)) { return false; }
+
+        auto user = std::make_shared<ClientSession>(std::move(client->socket()));
+        user->set_username(username);
+        m_users[username] = user;
+
+        return true;
+    }
+
+    void remove_user([[maybe_unused]] const std::string& username) {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_users.erase(username);
+    }
+
+    ClientPtr get_user([[maybe_unused]] const std::string& username) {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        auto it = m_users.find(username);
+        return (it != m_users.end()) ? it->second : nullptr;
+    }
+
+    std::vector<ClientPtr> get_all_users() {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        std::vector<ClientPtr> all_users;
+        for (auto it = m_users.begin(); it != m_users.end(); ++it) {
+            all_users.push_back(it->second);
+        }
+        return all_users;
+    }
+
+    void broadcast([[maybe_unused]] const std::string& message,
+                   [[maybe_unused]] const std::string& exclude = "") {
+        std::string formatted = "BROADCAST: " + message + "\n";
+
+        std::lock_guard<std::mutex> lock(m_mutex);
+        for (auto& [username, user] : m_users) {
+            if (username != exclude) {
+                try {
+                    boost::asio::write(user->socket(),
+                        boost::asio::buffer(formatted));
+                } catch (...) {}
+            }
+        }
+    }
+
+private:
+    std::unordered_map<std::string, ClientPtr> m_users;
+    std::mutex m_mutex;
+};
+
+class TcpChatServer {
+public:
+    TcpChatServer(boost::asio::io_context& io_context, uint16_t port = 9999)
+        : m_io_context(io_context)
+        , m_acceptor(io_context, tcp::endpoint(tcp::v4(), port))
+        , m_port(port)
+    {
+        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), port));
+        acceptor.set_option(tcp::acceptor::reuse_address(true));
+        acceptor.listen(128);
+        std::cout << "[Server] TCP Chat Server starting on port " << port << "...\n";
+    }
+
+    // ADD THIS
+    ~TcpChatServer() {
+
+    }
+
+    void start() {
+        m_acceptor.set_option(tcp::acceptor::reuse_address(true));
+
+    }
+
+    uint16_t port() const { return m_acceptor.local_endpoint().port(); }
+
+    UserRegistry& registry() { return m_registry; }
+
+private:
+    boost::asio::io_context& m_io_context;
+    tcp::acceptor m_acceptor;
+    uint16_t m_port;
+    UserRegistry m_registry;
+};
 
 inline int run_chat_server(uint16_t port) {
     try {
-        //boost::asio::io_context io_context;
-        //TcpChatServer server(io_context, port);
+        boost::asio::io_context io_context;
+        TcpChatServer server(io_context, port);
 
         std::cout << "[Server] Listening on port " << port << "...\n";
-        std::cout << "[Server] Press Ctrl+C to stop.\n\n";
 
-        //server.start();
+        server.start();
 
-        // Run the io_context (or implement your own threading model)
-        //io_context.run();
+        io_context.run();
 
         return 0;
 
